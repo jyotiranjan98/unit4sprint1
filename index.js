6ffd2e684aa1439c8eafa70f483bc5c813ca40f9
jyotiranjan98 <96143764+jyotiranjan98@users.noreply.github.com>
2022-03-07 13:27:37 +0530

    m

--- index.js
+++ index.js
@@ -9,30 +9,42 @@ tapp.get("/book",logger,(req,res)=>{
 })
 
 tapp.get("/libraries",logger, checkPermission("librarian"),(req,res) =>{
-    return res.send({route:"/libraries"})
+    return res.send({route:"/libraries",catching:true})
 })
 
 tapp.get("/authors",logger, checkPermission("author"),(req,res) =>{
-    return res.send({route:"/authors"})
+    return res.send({route:"/authors",catching:true})
 })
 
 
 function logger(req,res,next){
+    if(req.path=="/books"){
     console.log(req.path)
     next()
+    }
+    else if(req.path=="/libraries"){
+        console.log(req.path)
+        next() 
+    }
+    else if(req.path=="/authors"){
+        console.log(req.path)
+        next() 
+    }
 }
 
-function checkPermission(cat){
+function checkPermission(catching){
     return function logger1(req,res,next){
-        req.permissions=true
+        if(catching == "librarian"){
+            return next()
+        }
+       else if(catching =="author"){
+            return next()
+        }
+        else {
+            return false
+        }
     }
-    if(req.path=="/libraries"){
-        req.permissions=true
-    }
-    if(req.path=="/authors"){
-        req.permissions=true
-    }
-    next()
+    
 }
 
 tapp.listen(5000,()=>{
